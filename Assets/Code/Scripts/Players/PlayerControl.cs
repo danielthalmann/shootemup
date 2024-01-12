96967249ebaefee1b3449917599023eba234f553
Daniel Thalmann <daniel@thalmann.li>
2024-01-12 23:56:00 +0100

    clean player control

--- Assets/Code/Scripts/Players/PlayerControl.cs
+++ Assets/Code/Scripts/Players/PlayerControl.cs
@@ -9,7 +9,7 @@ public class PlayerControl : MonoBehaviour
     public GameObject projectile;
 
     public float speed = 0.2f;
-    public float interval = 1000.0f;
+    public float fire_interval = 1000.0f;
 
     private float elapse = 0;
 
@@ -19,34 +19,23 @@ public class PlayerControl : MonoBehaviour
     {
         PlayerInput = new PlayerInputActions();
         PlayerInput.Player.Enable();
-        //PlayerInput.Player.Fire.performed += Fire;
-
 
     }
-    /*
-    public void Fire(InputAction.CallbackContext context)
-    {
-                        
-        GameObject g = Instantiate(projectile, transform.position, transform.rotation);
-        Destroy(g, 5);
 
-    }
-    */
     private void FixedUpdate()
     {
 
         if (PlayerInput == null)
             Start();
 
-        Vector2 direction = PlayerInput.Player.Movement.ReadValue<Vector2>();
-        direction *= speed;
+        Vector2 direction = PlayerInput.Player.Movement.ReadValue<Vector2>() * speed;
         transform.Translate(direction);
 
         elapse += Time.deltaTime;
 
         if (PlayerInput.Player.Fire.IsPressed())
         {
-            if (elapse > interval) {
+            if (elapse > fire_interval) {
                 elapse = 0;
                 GameObject g = Instantiate(projectile, transform.position, transform.rotation);
                 Destroy(g, 3);
