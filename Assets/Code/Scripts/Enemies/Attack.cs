6d70f5366a16a1a9b9f238436ec0ba93bff8bd30
Daniel Thalmann <daniel@thalmann.li>
2024-01-22 23:04:16 +0100

    Fire direction player

--- Assets/Code/Scripts/Enemies/Attack.cs
+++ Assets/Code/Scripts/Enemies/Attack.cs
@@ -9,7 +9,18 @@ public class Attack : MonoBehaviour
 
     public float interval = 1.0f;
 
+    public GameObject projectile_position = null;
+    public bool target_player = false;
+
     private float time = 0f;
+
+    private void Start()
+    {
+        if (projectile_position == null)
+        {
+            projectile_position = this.gameObject;
+        }
+    }
 
 
     // Update is called once per frame
@@ -22,7 +33,22 @@ public class Attack : MonoBehaviour
 
         if (time > interval)
         {
-            GameObject obj = Instantiate(fire, transform.position, transform.rotation);
+            GameObject obj = Instantiate(fire, projectile_position.transform.position, projectile_position.transform.rotation);
+            VectorFollow vectorFollow = obj.GetComponent<VectorFollow>();
+            if (vectorFollow != null)
+            {
+                if (target_player)
+                {
+                    PlayerTarget target = FindFirstObjectByType<PlayerTarget>();
+                    if (target != null)
+                    {
+                        vectorFollow.direction = target.gameObject.transform.position - projectile_position.transform.position;
+                        vectorFollow.direction.Normalize();
+
+                    }
+                }
+
+            }
             Destroy(obj, 3);
             time = 0;
         }
